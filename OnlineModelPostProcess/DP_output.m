33198c2acf7c791b97717f2ad3b6fe50549b9e7a
sck79678 <sck79678@outlook.com>
2025-04-08 19:07:36 +0200

    Postprocessing Online
    Small Update

--- OnlineModelPostProcess/DP_output.m
+++ OnlineModelPostProcess/DP_output.m
@@ -1,13 +1,17 @@
 %% Structuralized output for DP
 close all; clear; clc; % close all other plots
-load('calmrun-0401-onlydata.mat');
+% load('calmrun-0401-onlydata.mat');
 % load('current10p-longitude-0401-onlydata.mat');
-% load('current10p-side-0401-2-fine-onlydata.mat');
+load('current10p-side-0401-2-fine-onlydata.mat');
 T_out=actual_pos_ned.time;
+t_cut_index=min(find(T_out>0.15)); % ignore the first 0.15 seconds
 Eta=actual_pos_ned.signals.values;
+T_out=T_out(t_cut_index:end);
+Eta=Eta(t_cut_index:end,:);
 T_Eta_Ref=desired_pos_ned.time;
 num=length(T_Eta_Ref);
 T_Eta_Ref_mat=zeros(2*num,1);
+T_Eta_Ref_mat(1)=T_out(1);
 T_Eta_Ref_mat(end)=T_out(end);
 Eta_Ref=desired_pos_ned.signals.values;
 Eta_Ref_mat=zeros(2*num,3);
@@ -28,7 +32,8 @@ for i=1:3
     xlim([0,T_out(end)]);
 end
 ax =subplot(3,2,2);
-config_string=sprintf("Experiment Condition = Calm Water\n");
+config_string=sprintf("Experiment Condition = 10%% Current (Side)\n");
+config_string=config_string+sprintf("Initial Position = (%.2f, %.2f, %.2f)\n",Eta(1,1),Eta(1,2),Eta(1,3));
 status_chr=["ON","OFF"];config_chr=["III","V"];
 % config_string=sprintf("StartingPoint = (%.2f, %.2f, %.2f)\n",Eta_initial(1),Eta_initial(2),Eta_initial(3));
 for i=1:num
@@ -42,7 +47,19 @@ config_string=config_string+sprintf("Using Thrust Allocation Config %s\n",config
 text(0,0.5,config_string);
 set (ax, 'visible', 'off')
 subplot(3,2,[4,6]);
-plot(Eta(:,2),Eta(:,1));xlabel('y');ylabel('x');
+legend_chars=repmat("Empty",num,1);
+for i=1:num
+    m1=find(T_out==T_Eta_Ref_mat(i*2-1));
+    m2=find(T_out==T_Eta_Ref_mat(i*2));
+    legend_chars(i)=sprintf("Point %d to Point %d",i-1,i);
+    plot(Eta(m1:m2,2),Eta(m1:m2,1));hold on;
+end
+for i=1:num
+    scatter(Eta_Ref(i,2),Eta_Ref(i,1),'filled');hold on;
+end
+scatter(Eta(1,2),Eta(1,1),'filled');
+legend(legend_chars,'Location','best');
+xlabel('y');ylabel('x');
 title("Trace (NED)");axis equal;grid on;
 dp_plot.Position=[100,100,900,500];
 
